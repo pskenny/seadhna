79a80dcb79c01a5f20970481d2e346aba53b3005
Paul Kenny <paul.s.kenny@gmail.com>
2020-09-22 05:15:37 +0100

    Add FeedItem, remove extra keystroke handling

--- src/main/java/io/github/pskenny/seadhna/ui/FeedItemsWindow.java
+++ src/main/java/io/github/pskenny/seadhna/ui/FeedItemsWindow.java
@@ -1,59 +1,80 @@
 package io.github.pskenny.seadhna.ui;
 
-import java.io.InterruptedIOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.Symbols;
 import com.googlecode.lanterna.gui2.ActionListBox;
 import com.googlecode.lanterna.gui2.BasicWindow;
-
-import io.github.pskenny.seadhna.io.NIOUtils;
 
 import com.sun.syndication.feed.synd.*;
 
 public class FeedItemsWindow extends BasicWindow {
     private HashSet<String> marked;
+    private ActionListBox actionListBox;
 
-    public FeedItemsWindow(SyndFeed feed, TerminalSize size) {
+    public FeedItemsWindow(SyndFeed feed) {
         marked = new HashSet<>();
+        actionListBox = new ActionListBox();
         List<SyndEntry> items = feed.getEntries();
 
-        ActionListBox actionListBox = new ActionListBox(size) {
-            // Add 'v' keystroke on this list to open feed item link in VLC
-            @Override
-            public Result handleKeyStroke(com.googlecode.lanterna.input.KeyStroke keyStroke) {
-                // Try and open VLC on 'v' input
-                if (keyStroke.getCharacter().equals(Character.valueOf('v'))) {
-                    // TODO handle indexoutofboundsexception,
-                    String url = items.get(this.getSelectedIndex()).getLink();
-                    // Check if list item at list index refers to a valid URL
-                    if (url != null && NIOUtils.isValidUrlPath(url)) {
-                        // Open VLC
-                        ProcessBuilder processBuilder = new ProcessBuilder("vlc",
-                                items.get(this.getSelectedIndex()).getLink());
-                        try {
-                            processBuilder.start();
-                        } catch (InterruptedIOException ex) {
-                            System.err.println("Couldn't open VLC");
-                        }
-                    }
-                }
-                return super.handleKeyStroke(keyStroke);
-            }
-        };
         // Add feed item titles to list and add action to add their links to the marked
         // list
-        items.forEach(entry -> actionListBox.addItem(entry.getTitle(), () -> 
-            marked.add(entry.getLink())));
+        items.forEach(entry -> actionListBox.addItem(
+            new FeedItem(entry.getTitle(), entry.getLink())
+        ));
 
         // Add Back button to open feeds window
         actionListBox.addItem("Back", this::close);
+
         setComponent(actionListBox);
     }
 
-    public Set getMarked() {
+    public Set<String> getMarked() {
+        actionListBox.getItems().iterator().forEachRemaining(item -> {
+            FeedItem fi = (FeedItem) item;
+            if (fi.isMarked()) {
+                marked.add(fi.getUrl());
+            }
+        });
         return marked;
+    }
+
+    private class FeedItem implements Runnable {
+        private boolean marked = false;
+        private String title;
+        private String url;
+
+        public FeedItem(String title, String url) {
+            this.title = title;
+            this.url = url;
+        }
+
+        @Override
+        public void run() {
+            toggleMarked();
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public boolean isMarked() {
+            return marked;
+        }
+
+        public void toggleMarked() {
+            marked = !marked;
+        }
+
+        @Override
+        public String toString() {
+            if (marked) {
+                return Symbols.BULLET + title;
+            } else {
+                return title;
+            }
+        }
     }
 }
